708c39b4fdb93ac3973b4c1c75540863ef1ba906
Akif Azwad <akif.azwad@bjitgroup.com>
2022-07-23 16:53:22 +0600

    added 2nd commit

--- src/StudentDetail.js
+++
@@ -1,9 +0,0 @@
-const StudentDetail = () => {
-    return ( 
-        <>
-        <h1>this is student detail</h1>
-        </>
-     );
-}
- 
-export default StudentDetail;
--- src/TeacherList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function TeacherList() {
-  return (
-    <div>TeacherList</div>
-  )
-}
-
-export default TeacherList
